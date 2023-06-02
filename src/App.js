739dbc0207ed683afe038e19d20a1994118723b5
abdalla mahmmoud <bodymoud1999@gmail.com>
2023-06-03 01:55:00 +0800

    A

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="background" />
+      <div className="content">
+       
+      </div>
+    </div>
+  );
+}
+
+export default App;
